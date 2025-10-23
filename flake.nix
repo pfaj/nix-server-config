67db69cb4790f54ce58194cb87c417ee690da690
Aidan Server <pfaj2004@gmail.com>
2025-10-22 21:48:13 -0400

    flake: updated flake and removed unneeded inputs

--- flake.nix
+++ flake.nix
@@ -8,98 +8,70 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      inputs.hyprland.follows = "hyprland";
-    };
-    hyprfocus = {
-      url = "github:pyt0xic/hyprfocus";
-      inputs.hyprland.follows = "hyprland";
-    };
-    ags = {
-      url = "github:Aylur/ags";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    quickshell = {
-      url = "github:outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    musnix = {url = "github:musnix/musnix";};
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    aylur-dotfiles.url = "github:bdebiase/aylur-dotfiles";
-    # nixvim = {
-    #   url = "github:nix-community/nixvim";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    zen-browser.url = "github:pfaj/zen-browser-flake";
-    niri = {
-      url = "github:YaLTeR/niri";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs: let
-    inherit (inputs.nixpkgs.lib) nixosSystem;
-    inherit (inputs.nixpkgs) lib;
-    system = "x86_64-linux";
+  outputs =
+    inputs:
+    let
+      inherit (inputs.nixpkgs.lib) nixosSystem;
+      inherit (inputs.nixpkgs) lib;
+      system = "x86_64-linux";
 
-    createSystem = username: hostname: configPath:
-      nixosSystem {
-        inherit system;
-        modules = [
-          configPath
-          ({pkgs, ...}: {
-            environment.systemPackages = with pkgs; [
-              (nixos-update {
-                configName = hostname;
-              })
-            ];
-          })
-        ];
-        specialArgs = {
-          inherit inputs;
-          inherit username;
+      createSystem =
+        username: hostname: configPath:
+        nixosSystem {
+          inherit system;
+          modules = [
+            configPath
+            (
+              { pkgs, ... }:
+              {
+                environment.systemPackages = with pkgs; [
+                  (nixos-update {
+                    configName = hostname;
+                  })
+                ];
+              }
+            )
+          ];
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+          };
         };
-      };
 
-    hostsDir = ././hosts;
-    hosts = let
-      dirs = builtins.attrNames (builtins.readDir hostsDir);
-      hostSystems = builtins.map (userDir: let
-        userPath = hostsDir + "/${userDir}";
-        deviceDirs = builtins.attrNames (builtins.readDir userPath);
-        systems =
-          builtins.map (deviceDir: let
-            devicePath = userPath + "/${deviceDir}";
-            configPath = devicePath + "/configuration.nix";
-            hostname = "${userDir}/${deviceDir}";
-          in {
-            "${hostname}" = createSystem userDir hostname configPath;
-          })
-          deviceDirs;
-      in
-        builtins.foldl' (a: b: a // b) {} systems)
-      dirs;
+      hostsDir = ././hosts;
+      hosts =
+        let
+          dirs = builtins.attrNames (builtins.readDir hostsDir);
+          hostSystems = builtins.map (
+            userDir:
+            let
+              userPath = hostsDir + "/${userDir}";
+              deviceDirs = builtins.attrNames (builtins.readDir userPath);
+              systems = builtins.map (
+                deviceDir:
+                let
+                  devicePath = userPath + "/${deviceDir}";
+                  configPath = devicePath + "/configuration.nix";
+                  hostname = "${userDir}/${deviceDir}";
+                in
+                {
+                  "${hostname}" = createSystem userDir hostname configPath;
+                }
+              ) deviceDirs;
+            in
+            builtins.foldl' (a: b: a // b) { } systems
+          ) dirs;
+        in
+        builtins.foldl' (a: b: a // b) { } hostSystems;
     in
-      builtins.foldl' (a: b: a // b) {} hostSystems;
-  in {
-    nixosConfigurations = hosts;
+    {
+      nixosConfigurations = hosts;
 
-    overlays = import ./overlays;
-    nixosModules = import ./modules/nixos;
-    homeManagerModules = import ./modules/home-manager;
-  };
+      overlays = import ./overlays;
+      nixosModules = import ./modules/nixos;
+      homeManagerModules = import ./modules/home-manager;
+    };
 }
